71a8eb4d6afc4e2f8287be6f91905a872fddf461
周伟 <wei.zhou@matrixport.com>
2023-07-01 19:39:18 +0800

    add fn

--- init.go
+++ init.go
@@ -3,6 +3,7 @@ package gconv
 import "os"
 
 func WriteToFile(v interface{}, fn string) {
+	os.Create(fn)
 	file, err := os.OpenFile(fn, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
