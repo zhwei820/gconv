96eb60f2935dfa12f6b12ae754c829a59e1668d5
周伟 <wei.zhou@matrixport.com>
2023-07-01 19:36:54 +0800

    WriteToFile

---
+++ init.go
@@ -0,0 +1,18 @@
+package gconv
+
+import "os"
+
+func WriteToFile(v interface{}, fn string) {
+	file, err := os.OpenFile(fn, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	SetExportExpand(true)
+
+	_, err = file.Write([]byte(Export(v)))
+	if err != nil {
+		panic(err)
+	}
+}
