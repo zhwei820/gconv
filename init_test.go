5e42caa176ab748a1e9ea80cb55b908184128b4b
周伟 <wei.zhou@matrixport.com>
2023-09-09 10:34:28 +0800

    u

--- init_test.go
+++ init_test.go
@@ -19,3 +19,18 @@ func TestWriteToFile(t *testing.T) {
 	LoadJsonFromFile(&a, "aa.json")
 	fmt.Println("a", Export(a))
 }
+
+func TestPlainWriteToFile(t *testing.T) {
+	a := struct {
+		A string
+		B int
+	}{
+		A: "a",
+		B: 1,
+	}
+	WritePlainToFile(a, "aa.json")
+	a.A = ""
+	a.B = 0
+	LoadPlainJsonFromFile(&a, "aa.json")
+	fmt.Println("a", Export(a))
+}
